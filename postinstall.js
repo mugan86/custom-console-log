82c2eca0e07f8cc3f82213c898662d6eae30fe45
Anartz Mugika <mugan86@gmail.com>
2018-12-11 20:07:49 +0100

    Update postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,5 @@
-console.log('🎉 🎉  \x1b[32m¡¡Librería correctamente instalada!!\x1b[0m 🎉 🎉');
+var emojis = require('./emojis');
+const head = emojis.large_orange_diamond;
+console.log(`${head.repeat(26)}`);
+console.log('  🎉 🎉  \x1b[32m¡¡Librería correctamente instalada!!\x1b[0m 🎉 🎉');
+console.log(`${head.repeat(26)}`);
