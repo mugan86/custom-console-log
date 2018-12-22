5aa2b3dfb1f0eaf7dee984a17cfd23892d60b7d4
Anartz Mugika <mugan86@gmail.com>
2018-12-22 09:58:02 +0100

    Formato para obtener la salida del log con los argumentos

---
+++ formatear-salida.js
@@ -0,0 +1,32 @@
+const mensajeTipo = 'estilo, mensaje,emoji, estilo,emoji';
+const contenido = 'background-color: red;';
+const value = mensajeTipo.split(',');
+let finalMessageFormat = '';
+for(let i = 0; i < value.length; i++) {
+    if (value[i].trim() === 'estilo') {
+        console.log(checkBlankSpaces(value[i]), '%c');
+        finalMessageFormat = finalMessageFormat + (checkBlankSpaces(value[i]) + '%c');
+    } else if (value[i].trim() === 'mensaje' || value[i].trim() === 'emoji') {
+        // checkBlankSpaces(value[i])
+        console.log(checkBlankSpaces(value[i]), '%s');
+        finalMessageFormat = finalMessageFormat + (checkBlankSpaces(value[i]) +  '%s');
+    } else {
+        // checkBlankSpaces(value[i])
+        console.log('ooo');
+    }
+
+    
+}
+
+console.log(finalMessageFormat);
+
+function checkBlankSpaces(text) {
+    let position = 0;
+    const blankSpacesStart = [];
+    while (text[position] === ' ' && position <= text.length) {
+        blankSpacesStart.push(' ');
+        position = position + 1;
+    }
+    console.log(blankSpacesStart.length);
+    return String(blankSpacesStart);
+}
